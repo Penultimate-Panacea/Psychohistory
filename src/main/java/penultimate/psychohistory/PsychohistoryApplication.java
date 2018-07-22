949b58b915ab3069de283517098ac437b748611c
Penultimate_Panacea <rian+github@fantozzi.org>
2018-07-22 15:52:28 -0400

    Initial Commit

---
+++ src/main/java/penultimate/psychohistory/PsychohistoryApplication.java
@@ -0,0 +1,14 @@
+package penultimate.psychohistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychohistoryApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Test String");
+	    SpringApplication.run(PsychohistoryApplication.class, args);
+
+	}
+}
