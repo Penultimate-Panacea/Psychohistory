72ae984652db3fab94a725665da5a9009acca6f7
Penultimate_Panacea <rian+github@fantozzi.org>
2018-07-22 22:04:36 -0400

    Initial data download tool created

---
+++ src/main/java/penultimate/psychohistory/DataUpdater.java
@@ -0,0 +1,77 @@
+package penultimate.psychohistory;
+
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+
+public class DataUpdater{
+
+    public void getFactionsFromURL(){
+        //Gets the initial Data Load for Factions
+        try {
+            URL website = new URL("https://eddb.io/archive/v5/factions.json");
+            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream("Data/factions.json");
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        }
+        catch (MalformedURLException e){
+            System.out.println("The URL provided for faction JSON is malformed.");
+        }
+        catch (FileNotFoundException e){
+            System.out.println("The factions.json file failed to be created by fos");
+        }
+        catch (IOException e){
+            System.out.println("Transfer Failed for factions.json");
+        }
+        finally {
+            System.gc();
+        }
+
+    }
+    public void getStationsFromURL(){
+        //Gets the initial Data Load for Stations
+        try {
+            URL website = new URL("https://eddb.io/archive/v5/stations.json");
+            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream("Data/stations.json");
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        }
+        catch (MalformedURLException e){
+            System.out.println("The URL provided for stations JSON is malformed.");
+        }
+        catch (FileNotFoundException e){
+            System.out.println("The stations.json file failed to be created by fos");
+        }
+        catch (IOException e){System.out.println("Transfer Failed for stations.json");}
+        finally {
+            System.gc();
+        }
+
+    }
+    public void getSystemsPopluatedFromURL(){
+        //Gets the initial Data Load for Systems_Populated
+        try {
+            URL website = new URL("https://eddb.io/archive/v5/systems_populated.json");
+            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream("Data/systems_populated.json");
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        }
+        catch (MalformedURLException e){
+            System.out.println("The URL provided for systems_populated JSON is malformed.");
+        }
+        catch (FileNotFoundException e){
+            System.out.println("The systems_populated.json file failed to be created by fos");
+        }
+        catch (IOException e){System.out.println("Transfer Failed for factions.json");}
+        finally {
+            System.gc();
+        }
+
+    }
+}
--- src/main/java/penultimate/psychohistory/PsychohistoryApplication.java
+++ src/main/java/penultimate/psychohistory/PsychohistoryApplication.java
@@ -3,12 +3,24 @@ package penultimate.psychohistory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PsychohistoryApplication {
 
 	public static void main(String[] args) {
-        System.out.println("Test String");
-	    SpringApplication.run(PsychohistoryApplication.class, args);
+        System.out.println("Initalized");
+		DataUpdater du = new DataUpdater();
+		System.out.println("Starting Factions");
+		du.getFactionsFromURL();
+		System.out.println("Complete Factions");
+		System.out.println("Starting Stations");
+		du.getStationsFromURL();
+		System.out.println("Complete Stations");
+		System.out.println("Starting SystemsPopulated");
+		du.getSystemsPopluatedFromURL();
+		System.out.println("Complete SystemsPopulated");
+		System.out.println("Completed");
+	    //SpringApplication.run(PsychohistoryApplication.class, args);
 
 	}
 }
